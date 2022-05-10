25e625584658f5be28de28b45624955674e7a43f
alexander-hagg <alexander.hagg@h-brs.de>
2022-05-10 19:12:14 +0200

    Some comments

--- dev_mapelites.py
+++ dev_mapelites.py
@@ -1,13 +1,17 @@
 import sys
 import yaml
+
+# Load QD algorithm
 sys.path.append('qd/mapelites')
 from mapelites import *
 from vis_archive import *
-from domain.random import fitness_fun as ff
-
 config = yaml.safe_load(open("qd/mapelites/config.yml"))
+
+# Load domain
+from domain.random import fitness_fun as ff
 domain = yaml.safe_load(open("domain/random/domain.yml"))
 
+# Run QD and visualize
 archive = mapelites(config, domain, ff)
 vis_archive(archive)
 
