86c639e04d83926e1a0afbc1ecee4309e58440a6
Alexander Hagg <info@haggdesign.de>
2022-05-18 14:41:02 +0200

    Improved VE performance by removing nonzero checks in update map

--- dev_voronoielites.py
+++ dev_voronoielites.py
@@ -11,9 +11,14 @@ config = yaml.safe_load(open("qd/voronoielites/config.yml"))
 domain, random_pop = init.do(config.get('init_samples'))
 
 # Run QD, time it and visualize
+# import cProfile
+# cProfile.run('qd.evolve(random_pop, config, domain, fitness_fun)', 'restats')
+# import pstats
+# from pstats import SortKey
+# p = pstats.Stats('restats')
+# p.sort_stats(SortKey.TIME).print_stats(10)
 start = time.time()
 archive = qd.evolve(random_pop, config, domain, fitness_fun)
 print(f'Time elapsed: {time.time() - start:.2}s.')
 # visualize.plot(archive, domain)
 visualize_phenotypes.plot(archive, express, domain, config)
-
--- qd/voronoielites/update_archive.py
+++ qd/voronoielites/update_archive.py
@@ -8,22 +8,11 @@ def update_archive(archive, genes, features, fitness, config, domain):
     all_fitness = np.vstack((archive['fitness'],fitness))
     while all_features.shape[0] > config['resolution']:
         distances = cdist(all_features, all_features)
-        
-        #x, y = np.argmin(distances[np.nonzero(distances)], keepdims=True)
-        ind = np.unravel_index(np.argmin(distances[np.nonzero(distances)], axis=None), distances.shape)
+        ind = np.unravel_index(np.argmin(distances[np.where(~np.eye(distances.shape[0],dtype=bool))], axis=None), distances.shape)
         lower = all_fitness[ind[0]] > all_fitness[ind[1]]
-        
-        # print(f'all_features shape: {all_features.shape}')
         all_genomes = np.delete(all_genomes, ind[int(lower)], 0)
         all_features = np.delete(all_features, ind[int(lower)], 0)
         all_fitness = np.delete(all_fitness, ind[int(lower)], 0)
-        # print(f'all_features shape: {all_features.shape}')
-        
-        # print(ind)
-        # print(int(higher))
-        # print(ind[int(higher)])
-        # replaced.append(ind[int(not higher)])
-        # replacement.append(ind[int(higher)])
 
     # Update archive
     archive.update({'genes': all_genomes})
