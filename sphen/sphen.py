b7838f72791cbfedd1bb003466a3c38a3928b5c7
alexander-hagg <alexander.hagg@h-brs.de>
2022-05-19 14:48:38 +0200

    SPHEN: In feature space, select nearest neighbours of Sobol emitters

--- sphen/sphen.py
+++ sphen/sphen.py
@@ -2,9 +2,12 @@ import numpy as np
 import yaml
 import GPy
 from IPython.display import display
+from scipy.stats import qmc
 
 import sphen.surrogates as surrogates
 from qd.voronoielites import qd
+from scipy.spatial.distance import cdist
+
 
 def evolve(samples, config, domain, ff):
     # Get true values for sample set
@@ -15,6 +18,9 @@ def evolve(samples, config, domain, ff):
     # Setup internal QD algorithm
     qdconfig = yaml.safe_load(open("qd/voronoielites/config.yml"))
     
+    # Setup Sobol sampler
+    sampler = qmc.Sobol(d=domain['nfeatures'], scramble=True)
+            
     # Sampling loop
     while observation.shape[0] <= total_samples:
         print(f'Current samples: {observation.shape[0]}/{total_samples}')
@@ -27,7 +33,11 @@ def evolve(samples, config, domain, ff):
         archive = qd.evolve(observation, qdconfig, domain, ucbfitfun)
         
         # Select infill samples
-        selection = np.random.randint(0, high=archive['genes'].shape[0], size=config['num_samples'])
+        # Create emitter points using Sobol sequence and select closest points in the set (feature-based)
+        emitter_points = sampler.random(config['num_samples'])
+        distances = cdist(emitter_points, features)
+        selection = np.argmin(distances, axis=1)
+        
         sel_observation = archive['genes'][selection]
         sel_fitness, sel_features = ff.get(sel_observation, domain)
         observation = np.vstack([observation, sel_observation])
