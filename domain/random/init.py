4e8fe6d2785bb369cd74370f2ffd3fdc8fdbf13d
alexander-hagg <alexander.hagg@h-brs.de>
2022-05-19 14:32:41 +0200

    Removed random domain which was only used for debugging ME/VE

--- domain/random/init.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import yaml
-
-def do(ninit_samples):
-    domain = yaml.safe_load(open("domain/random/domain.yml"))
-    random_pop = np.random.rand(ninit_samples,domain['dof'])
-    return domain, random_pop
