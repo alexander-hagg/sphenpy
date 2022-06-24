11541de4bd6055a7ed957f2b877937c711310441
alexander-hagg <alexander.hagg@h-brs.de>
2022-06-24 22:17:20 +0200

    Vary height

--- domain/nsg_simple/express.py
+++ domain/nsg_simple/express.py
@@ -15,28 +15,26 @@ def express_single(genome, domain):
     if (np.isnan(genome)).any():
         return None
     x, y, z = np.indices((domain['plot_size'], domain['plot_size'], 4))
-    for i in range(int(domain['dof']/2)):
-        pos_x = genome[i*2]
-        pos_y = genome[i*2+1]
+    for i in range(int(domain['dof'])):
+        pos_x = genome[i*3]
+        pos_y = genome[i*3+1]
+        height = genome[i*3+2]
+        # print(height)
         pheno_x = (x > (pos_x)) & (x < (pos_x + domain['square_size'][0]))
         pheno_y = (y > (pos_y)) & (y < (pos_y + domain['square_size'][1]))
         floorplan = pheno_x & pheno_y
-        z_offset = 0
-        if i > 0:
-            if np.sum(floorplan & total_phenotype) > 0:
-                print(np.sum(floorplan & total_phenotype))
-                z_offset += 1
 
-        building_height = (z > z_offset) & (z < (z_offset + 2))
+        building_height = (z > 0) & (z < (height+1))
         phenotype = (floorplan & building_height)
-        
+
         if i == 0:
             total_phenotype = phenotype
         else:
             total_phenotype = total_phenotype | phenotype
     return total_phenotype
 
-def visualize_raw(phenotype, color=[0.7, 0.7, 0.7], dx=0, dy=0):
+
+def visualize_raw(phenotype, color=[0.5, 1, 0.5], dx=0, dy=0):
     ax = plt.figure().add_subplot(projection='3d')
     ax.voxels(phenotype, facecolors=color, edgecolor='k')
 
@@ -44,7 +42,7 @@ def visualize_raw(phenotype, color=[0.7, 0.7, 0.7], dx=0, dy=0):
 
 def visualize(phenotype, domain):
     visualize_raw(phenotype)
-    # plt.axis('auto')
+    plt.axis('auto')
     ax = plt.gca()
     ax.set_xlim([0, domain['plot_size']])
     ax.set_ylim([0, domain['plot_size']])
--- domain/nsg_simple/init.py
+++ domain/nsg_simple/init.py
@@ -7,13 +7,14 @@ from scipy.stats import qmc
 def do(ninit_samples):
     domain = yaml.safe_load(open("domain/nsg_simple/domain.yml"))
 
-    domain['par_ranges'] = [domain['par_ranges'][0] * np.ones(domain['dof']), domain['par_ranges'][1] * np.ones(domain['dof'])]
-    
-    sampler = qmc.Sobol(d=domain['dof'], scramble=True)
+    domain['par_ranges'] = np.tile(domain['par_ranges'], math.floor(domain['dof']))
+    ranges = np.asarray(domain['par_ranges'])
+
+    sampler = qmc.Sobol(d=domain['dof']*3, scramble=True)
     random_pop = sampler.random(ninit_samples)
     # Project sampling to parameter ranges
-    ranges = np.asarray(domain['par_ranges'])
     random_pop = np.multiply(random_pop, (ranges[1] - ranges[0])) + ranges[0]
-    # random_pop = np.floor(random_pop)
+    random_pop = np.round(random_pop)
+    print(random_pop.size)
 
     return domain, random_pop
