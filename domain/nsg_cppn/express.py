6aaad4013ac568fda915c252eda097eaa6b3a6a9
alexander-hagg <alexander.hagg@h-brs.de>
2022-07-13 16:58:21 +0200

    First static CPPN properly visualized

--- domain/nsg_cppn/express.py
+++ domain/nsg_cppn/express.py
@@ -3,15 +3,16 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 import numpy as np
 import util.voxCPPN.tools as voxvis
+import math
 
 def do_surf(genomes, domain):
     phenotypes = []
     for i in range(genomes.shape[0]):
-        phenotype = express_single_surface(genomes[i], domain)
+        phenotype = cppn_out(genomes[i], domain)
         phenotypes.append(phenotype)
     return phenotypes
 
-def express_single_surface(genome, domain):
+def cppn_out(genome, domain):
     if (np.isnan(genome)).any():
         return None
     X = np.arange(0, domain['grid_length'], 1)
@@ -19,8 +20,11 @@ def express_single_surface(genome, domain):
     X, Y = np.meshgrid(X, Y)
 
     # TODO This should be replaced with a true CPPN (genome)
-    Z = 3*np.sin(np.sqrt((X/10)**2 + (Y/10)**2))
+    Z = 3*np.sin(np.sqrt((X/2)**2 + (Y/2)**2))
+    # Z[:] = 2
+    # Z = 1*np.mod(X,4)+2
     phenotype = [X,Y,Z]
+    print(X.shape)
 
     return phenotype
 
@@ -34,27 +38,18 @@ def do(genomes, domain):
 
 
 def express_single(genome, domain):
-    if (np.isnan(genome)).any():
-        return None
-    X = np.arange(0, domain['grid_length'], 1)
-    Y = np.arange(0, domain['grid_length'], 1)
-    X, Y = np.meshgrid(X, Y)
-
-    # TODO This should be replaced with a true CPPN (genome)
-    Z = 3*np.sin(np.sqrt((X/10)**2 + (Y/10)**2))
-    phenotype = [X,Y,Z]
+    X, Y, Z = cppn_out(genome, domain)
     # Convert to voxels
     print(f'Z: {Z}')
     voxels = np.zeros([domain['grid_length'], domain['grid_length'], domain['grid_length']])
     for x in range(domain['grid_length']):
         for y in range(domain['grid_length']):
             height = np.rint(Z[x,y])
-            print(height)
             for z in range(int(height)):
-                voxels[x, y, int(height)-z] = 1
+                print(f'{x}/{y}/{z}/')
+                voxels[x, y, math.floor(z)] = 1
     # voxels = np.rint(outputs).reshape(args.size, args.size, args.size)
-    print(f'voxels: {voxels}')
-    
+
     return voxels
 
 
@@ -70,5 +65,8 @@ def visualize(phenotype, domain):
     phenotype = phenotype.astype('int')
     phenotype = np.transpose(phenotype, axes=[1, 2, 0])
     phenotype = np.flip(phenotype, axis=1)
+    print(f'voxels: {phenotype}')
+    print(f'voxels.shape: {phenotype.shape}')
+
     np.save('tmp', phenotype)
-    voxvis.render_voxels(phenotype)
+    voxvis.render_voxels(np.pad(phenotype, 1, mode='empty'))
