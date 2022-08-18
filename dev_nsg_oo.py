490376930bcbdf47b7f574412f1ead2540bb9ccb
alexander-hagg <alexander.hagg@h-brs.de>
2022-08-18 11:44:27 +0200

    Renamed Rastrigin domain after testing

--- dev_nsg_oo.py
+++ dev_nsg_oo.py
@@ -9,22 +9,12 @@ from qd.mapelites import evolution, visualize, visualize_phenotypes
 
 config = yaml.safe_load(open("qd/mapelites/config.yml"))
 domain, random_pop = init.do(config['init_samples'])
-# phenotypes = [x.express() for x in random_pop]
-# [x.mutate(config['mut_probability'], config['mut_sigma']) for x in random_pop]
-# phenotypes = [x.express() for x in random_pop]
-# fitness, features, phenotypes = fitness_function.get(random_pop, domain)
-
-# plt = express.visualize_pyvista(phenotypes, domain, features)
 
 start = time.time()
 fitfun = lambda x: fitness_function.get(x, domain)
 archive = evolution.evolve(random_pop, config, domain, fitfun)
 print(f'Time elapsed: {time.time() - start:.2}s.')
 archive.plot()
-# visualize.plot(archive, domain)
 
-# archive_geneslist = np.squeeze((archive['genes'])).flatten().tolist()
-# # print(archive_geneslist)
 # fitness, features, phenotypes = fitness_fun.get(archive_geneslist, domain)
 # plt = express.visualize_pyvista(phenotypes, domain, features, fitness)
-# # visualize_phenotypes.plot(archive, express, domain, config)
--- domain/rastrigin/init.py
+++ domain/rastrigin/init.py
@@ -1,11 +1,13 @@
 import numpy as np
 import yaml
 
+from domain.ootest.genome import cppn_genome
 
 def do(ninit_samples):
-    domain = yaml.safe_load(open("domain/rastrigin/domain.yml"))
-    random_pop = np.random.rand(ninit_samples,domain['dof'])
-    # Project sampling to parameter ranges
-    ranges = np.asarray(domain['par_ranges'])
-    random_pop = np.multiply(random_pop, (ranges[1] - ranges[0])) + ranges[0]
+    domain = yaml.safe_load(open("domain/ootest/domain.yml"))
+
+    random_pop = []
+    for i in range(ninit_samples):
+        random_pop.append(cppn_genome(domain))
+
     return domain, random_pop
